521dd55aac6917a8e975dc880fb4fa1952395b59
Alberto Tudela <ajtudela@gmail.com>
2024-11-13 19:53:26 +0100

    Update4
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- .github/stage-sg/01-configs/01-run-chroot.sh
+++ .github/stage-sg/01-configs/01-run-chroot.sh
@@ -3,4 +3,7 @@
 # Cloning git repository
 echo 'Cloning git repository'
 cd /home/pi
-sudo -u "${FIRST_USER_NAME}" git clone https://github.com/jonnerd154/StargateProject-software sg1_v4
+#sudo -u "${FIRST_USER_NAME}" git clone https://github.com/jonnerd154/StargateProject-software sg1_v4
+git clone -b ci https://github.com/ajtudela/StargateProject-software sg1_v4
+sudo chown -R "${FIRST_USER_NAME}":"${FIRST_USER_NAME}" sg1_v4
+ls -la
